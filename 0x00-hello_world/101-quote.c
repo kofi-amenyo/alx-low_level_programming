6eba1ca50e9be78fef21bcb4194688a080527225
kofi-amenyo <mawuli.amenyo@amalitech.org>
2022-03-11 00:38:01 +0000

    file 1 2 and 101quote coorections.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-i#include <stdio.h>
+#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Prints out the last part of a quote in the standard error.
@@ -6,6 +6,7 @@ i#include <stdio.h>
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
+
